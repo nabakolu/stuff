017355ba8578d938be2883a54676673055ac79ff
nabakolu <lnabakowski@mailo.com>
2021-08-19 14:08:00 +0200

    Updated urlopener

--- usr/local/bin/urlopener.sh
+++ usr/local/bin/urlopener.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-choice=$(echo "Web Browser
-mpv" | dmenu -l 20)
 
-if [ "$choice" = "Web Browser" ]; then
-	choice="xdg-open"
+if [ "$1" = "" ]
+then
+	file=$(xclip -o -selection clip-board)
+else
+	file=$1
 fi
 
-file=$(xclip -o -selection clip-board)
-$choice $file
+case "$file" in
+	*mkv|*webm|*mp4|*youtube.com/watch*|*youtube.com/playlist*|*youtu.be*)
+		setsid -f mpv -quiet "$file" >/dev/null 2>&1 ;;
+	*png|*jpg|*jpe|*jpeg|*gif)
+		curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && sxiv -a "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+	*pdf)
+		curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && mupdf "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+	*)
+		[ -f "$file" ] && setsid -f "$TERMINAL" -e "$EDITOR" "$file" >/dev/null 2>&1 || setsid -f "$BROWSER" "$file" >/dev/null 2>&1
+esac
