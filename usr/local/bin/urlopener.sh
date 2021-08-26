0fe8881d9eb233b19f6582b636e51b52bb524772
nabakolu <lnabakowski@mailo.com>
2021-08-26 15:59:07 +0200

    Made urlopener wget mp3 files

--- usr/local/bin/urlopener.sh
+++ usr/local/bin/urlopener.sh
@@ -8,16 +8,21 @@ else
 fi
 
 case "$file" in
+
+	### Videos in mpv
 	*mkv|*webm|*mp4|*youtube.com/watch*|*youtube.com/playlist*|*youtu.be*)
 		setsid -f mpv -quiet "$file" >/dev/null 2>&1 ;;
+
+	### Audio: download mp3 files, youtube-dl from soundcloud and play in xdg-open
+	*http*://*.mp3)
+		wget $file -P /tmp/
+		xdg-open /tmp/$(basename $file) ;;
 	*soundcloud.com*)
 		youtube-dl -o "/tmp/%(title)s.%(ext)s" $file --exec xdg-open ;;
 	*png|*jpg|*jpe|*jpeg|*gif)
 		curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && sxiv -a "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
 	*pdf)
 		curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && mupdf "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
-	*mp3)
-		setsid -f st -e mocp "$file" >/dev/null 2>&1 ;;
 	*)
 		[ -f "$file" ] && setsid -f "$TERMINAL" -e "$EDITOR" "$file" >/dev/null 2>&1 || setsid -f "$BROWSER" "$file" >/dev/null 2>&1
 esac
