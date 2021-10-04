687ee13eb8d8b3b3df84e72f805bb3d8e8928457
nabakolu <lnabakowski@mailo.com>
2021-10-04 12:44:18 +0200

    Made change to picom toggle such that is POSIX compliant when using nohup

--- usr/local/bin/toggle_p.sh
+++ usr/local/bin/toggle_p.sh
@@ -3,7 +3,7 @@
 
 COUNT=$(pgrep -c picom)
 if [ "$COUNT" = "0" ]; then
-	nohup picom --experimental-backends 0<&- &>/dev/null &
+	nohup picom --experimental-backends > /dev/null 2>&1&
 else
 	killall picom
 fi
