a65ad05734b04a24fbdc8ed2c0234405e66e0cc5
nabakolu <lnabakowski@mailo.com>
2024-09-12 23:28:54 +0200

    Use spotdl to get audio from spotify links in urlopener Add spotdl to aurneed

--- usr/local/bin/urlopener.sh
+++ usr/local/bin/urlopener.sh
@@ -2,27 +2,29 @@
 
 if [ "$1" = "" ]
 then
-	file=$(xclip -o -selection clip-board)
+    file=$(xclip -o -selection clip-board)
 else
-	file=$1
+    file=$1
 fi
 
 case "$file" in
+    http*://*spotify.com/*)
+        spotdl url $file | tail -n +3 > /tmp/spotdl
+        setsid -f "$BROWSER" "$(cat /tmp/spotdl)" >/dev/null 2>&1 ;;
+    ### Videos in mpv
+    *.mkv|*.webm|*.mp4|*youtube.com/watch*|*youtu.be/*|*youtube.com/playlist*|*youtu.be/watch*|*ardmediathek.de/video*|*v.redd.it/*|*yewtu.be/watch*)
+        nohup setsid -f mpv -quiet "$file" >/dev/null 2>&1 ;;
 
-	### Videos in mpv
-	*.mkv|*.webm|*.mp4|*youtube.com/watch*|*youtu.be/*|*youtube.com/playlist*|*youtu.be/watch*|*ardmediathek.de/video*|*v.redd.it/*|*yewtu.be/watch*)
-		nohup setsid -f mpv -quiet "$file" >/dev/null 2>&1 ;;
-
-	### Audio: download mp3 files, youtube-dl from soundcloud and play in xdg-open
-	http*://*.mp3)
-		wget $file -O /tmp/$(basename $file) -nc
-		$TERMINAL -e mocp /tmp/$(basename $file) ;;
-	*soundcloud.com*)
-		yt-dlp -o "/tmp/%(title)s.%(ext)s" $file --exec xdg-open ;;
-	*png|*jpg|*jpe|*jpeg|*gif)
-		curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && sxiv -a "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
-	*pdf)
-		curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && mupdf "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
-	*)
-		setsid -f "$BROWSER" "$file" >/dev/null 2>&1
+    ### Audio: download mp3 files, youtube-dl from soundcloud and play in xdg-open
+    http*://*.mp3)
+        wget $file -O /tmp/$(basename $file) -nc
+        $TERMINAL -e mocp /tmp/$(basename $file) ;;
+    *soundcloud.com*)
+        yt-dlp -o "/tmp/%(title)s.%(ext)s" $file --exec xdg-open ;;
+    *png|*jpg|*jpe|*jpeg|*gif)
+        curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && sxiv -a "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+    *pdf)
+        curl -sL "$file" > "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")" && mupdf "/tmp/$(echo "$file" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+    *)
+        setsid -f "$BROWSER" "$file" >/dev/null 2>&1
 esac
